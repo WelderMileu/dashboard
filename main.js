1292e74e3b373343994c79735d6bdc29330bcc0e
WelderMileu <welderfelix21@gmail.com>
2020-02-06 23:04:08 -0300

    Inserindo o primeiro grafico

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 (function(){
-	// code ...
+	"use strict";
+	
+	// Grafico 01
+	const grafic01 = document.getElementById('grafic01');
+	const myChart = new Chart(grafic01,{
+		type:'bar',
+		data:{
+			labels:['Red', 'Blue', 'Orange','Yellow','Green','Purple', 'Orange'],
+			datasets:[{
+				label:'# of votes',
+				data:[12,19,3,5,2,3],
+				backgroundColor: [
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)'
+            	],
+            	borderColor: [
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)',
+                	'rgba(54, 162, 235, 0.2)'
+            	],
+            	borderWidth: 1
+        	}]
+			
+		},
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	})
+
 })();
