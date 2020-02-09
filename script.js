743d0eb71e3dcb21a98bab51ddc1a540717ca16e
WelderMileu <welderfelix21@gmail.com>
2020-02-08 22:47:38 -0300

    Manipulando menus com javascript

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 (function() {
 	'use strict';
 
+	// Selector
 	const $ = (event) =>  document.querySelector(event);
 
-
+	// Menu do header.
 	$('#toggle-header-menu').addEventListener('click',() => {
 		$('.navbar').classList.toggle('toggle-header');
+	});
+
+	// Menu do usuario.
+	$('#toggle-menu').addEventListener('click',() => {
+		$('.menu').classList.toggle('toggle-menu-menu');
 	});
 
 })();
